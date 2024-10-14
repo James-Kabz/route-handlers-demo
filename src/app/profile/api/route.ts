ad4d1551cbf310b71c8bcd641369978073a889e1
James-Kabz <kabogp@gmail.com>
2024-10-14 10:50:21 +0300

    learnt a few concepts,headers in route handlers,response handlers,cookies in route handlers,carching route handlers,middlewares and alos rendering with SSG and SSR and the drawbacks

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,3 +1,25 @@
-export async function GET() {
-    return new Response("Profile Api data");
+import { type NextRequest } from "next/server"
+import { headers,cookies } from "next/headers"
+
+export async function GET(request:NextRequest) {
+
+    const requestHeaders = new Headers(request.headers);
+    const headerList = headers();
+    cookies().set("resultsPerPage","20");
+
+
+    console.log(requestHeaders.get("Authorization"));
+    console.log(headerList.get("Authorization"))
+    console.log(cookies().get("resultsPerPage"))
+
+    const theme = request.cookies.get("theme")
+
+    console.log(theme )
+    
+    return new Response("<h1>Profile API data</h1>",{
+        headers : {
+            "Content-Type" : "text/html",
+            "Set-Cookie" : "theme = dark"
+        },
+    });
 }
